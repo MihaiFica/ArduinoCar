7d27bf4158fc9371fa5de9cd740ffbd8ab53dcc7
Mihai Fica <45798163+MihaiFica@users.noreply.github.com>
2019-02-21 22:08:34 +0200

    Update car_arduino2.ino

--- car_arduino2.ino
+++ car_arduino2.ino
@@ -1,11 +1,11 @@
- #include <SoftwareSerial.h>
+#include <SoftwareSerial.h>
  SoftwareSerial BTSerial(2, 4);
  #include <Servo.h>
  const int servoMotor = 5;
- const int motorA1  = 8;  
- const int motorA2  = 9; 
- const int motorB1  = 10; 
- const int motorB2  = 11;
+ const int motorA1 = 8;  
+ const int motorA2 = 9; 
+ const int motorB1 = 10; 
+ const int motorB2 = 11;
  const int trigPin = 3;
  const int echoPin = 6;
  long durata;
@@ -27,73 +27,69 @@ void setup()
     
 }
 
-void loop() {
-  
-   digitalWrite(trigPin, LOW);
-   delayMicroseconds(2); 
-   digitalWrite(trigPin, HIGH);
-   delayMicroseconds(10);
-   digitalWrite(trigPin, LOW);
-   durata = pulseIn(echoPin, HIGH);
-   distanta= durata*0.034/2;
-   Serial.println(distanta);
-   if(distanta<15)
-   {      
-         while(distanta<30)
-         { 
-    
-           digitalWrite(motorA1,HIGH);
-           digitalWrite(motorA2,LOW);
-           digitalWrite(motorB1,LOW);
-           digitalWrite(motorB2,HIGH);
-           digitalWrite(trigPin, HIGH);
-           delayMicroseconds(10);
-           digitalWrite(trigPin, LOW);
-           durata = pulseIn(echoPin, HIGH);
-           distanta= durata*0.034/2;
-           Serial.println(distanta);                         
-          }
-          stare = 'O';
-   }
-   
-       
-      if(BTSerial.available() > 0)
-         {     
-           stare = BTSerial.read(); 
-           Serial.println(stare);  
+void loop() 
+{
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2); 
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    durata = pulseIn(echoPin, HIGH);
+    distanta = durata * 0.034 / 2;
+    Serial.println(distanta);
+    if(distanta < 15)                                                   //algoritm prin care se evita ciocnirea masinii cu un obstacol
+        {                                                               //daca masina ajunge la 15 centimetrii de un obstocol motoarele isi schimba sensul de rotatie ,iar masina se departeaza de obstacol
+            if(distanta < 40)                                           //in acest timp controlul nu este la utilizator  
+                { 
+                    digitalWrite(motorA1, HIGH);
+                    digitalWrite(motorA2, LOW);
+                    digitalWrite(motorB1, LOW);
+                    digitalWrite(motorB2, HIGH);
+                    digitalWrite(trigPin, HIGH);
+                    delayMicroseconds(10);
+                    digitalWrite(trigPin, LOW);
+                    durata = pulseIn(echoPin, HIGH);
+                    distanta = durata * 0.034 / 2;
+                    Serial.println(distanta);                         
+                }
+             stare = 'O';
          }
-    
-      if(stare == 'A' )
+    if(BTSerial.available() > 0)
+         {     
+             stare = BTSerial.read(); 
+             Serial.println(stare);  
+         }
+    if(stare == 'A' )                                                    //mers inainte
+         {
+             digitalWrite(motorA1, LOW);
+             digitalWrite(motorA2, HIGH);
+             digitalWrite(motorB1, HIGH);
+             digitalWrite(motorB2, LOW);
+         }
+    if(stare == 'O')                                                     //echilibru motoare
+         {
+             digitalWrite(motorA1, LOW);
+             digitalWrite(motorA2, LOW);
+             digitalWrite(motorB1, LOW);
+             digitalWrite(motorB2, LOW);
+         }
+    if(stare == 'D')                                                     //mers inapoi
+         {
+             digitalWrite(motorA1, HIGH);
+             digitalWrite(motorA2, LOW);
+             digitalWrite(motorB1, LOW);
+             digitalWrite(motorB2, HIGH);
+         }
+    if(stare == 'B')
+         {
+             myservo.write(90);                                        //viraj dreapta
+         }
+    if(stare == 'C')
         {
-           digitalWrite(motorA1,LOW);
-           digitalWrite(motorA2,HIGH);
-           digitalWrite(motorB1,HIGH);
-           digitalWrite(motorB2,LOW);
+             myservo.write(30);                                        //viraj stanga
         }
-      if(stare == 'O')
+    if(stare == 'H')
         {
-           digitalWrite(motorA1,LOW);
-           digitalWrite(motorA2,LOW);
-           digitalWrite(motorB1,LOW);
-           digitalWrite(motorB2,LOW);
-        }
-      if(stare == 'D')
-        {
-           digitalWrite(motorA1,HIGH);
-           digitalWrite(motorA2,LOW);
-           digitalWrite(motorB1,LOW);
-           digitalWrite(motorB2,HIGH);
-        }
-      if(stare == 'B')
-        {
-           myservo.write(90);
-        }
-      if(stare == 'C')
-        {
-           myservo.write(30);
-        }
-      if(stare == 'H')
-        {
-           myservo.write(60);
+             myservo.write(60);                                        // echilbru servo
         }
 }
